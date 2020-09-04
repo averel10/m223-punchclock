916bd3229819eec7e8b58e6d6d9fdec4c73971d1
Achille <achille.huenenberger@gmail.com>
2020-09-04 11:04:50 +0200

    added username unique

--- src/main/java/ch/zli/m223/punchclock/domain/User.java
+++ src/main/java/ch/zli/m223/punchclock/domain/User.java
@@ -1,9 +1,8 @@
 package ch.zli.m223.punchclock.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.hibernate.validator.constraints.UniqueElements;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -12,6 +11,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @NotNull
+    @Column(unique = true)
     private String username;
     @NotNull
     private String password;
